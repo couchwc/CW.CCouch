be854fd84c38c6e4ac3920900947a5bd741dfefc
Cody Couch <couchwc@users.noreply.github.com>
2022-03-23 14:05:15 -0400

    Pushing up exercise into the seeding project

---
+++ src/CW.CCouch.Seeding/Exersices/RGBToHexConversion.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CW.CCouch.Seeding.Exersices
+{
+
+/// <summary>
+/// 
+/// </summary>
+    internal class RGBToHexConversion
+    {
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public void Exercise()
+        {
+            
+            const string WhiteHexColor =
+                "FFFFFF";
+
+            var value =
+                CW.CCouch.SharedKernel.Colors.RGB.ToHexidecimal(
+                    r: 255,
+                    g: 255,
+                    b: 255);
+
+            var areEqual = string.Equals(
+                a: WhiteHexColor,
+                b: value);
+
+        }
+
+    }
+
+}
