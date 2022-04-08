c39867c2c0659b9154784f9009211db3ae34fbe4
Cody Couch <couchwc@users.noreply.github.com>
2022-04-08 15:34:16 -0400

    Implementing the following exercise: https://www.codewars.com/kata/5174a4c0f2769dd8b1000003/train/csharp

--- src/CW.CCouch.Seeding/Program.cs
+++ src/CW.CCouch.Seeding/Program.cs
@@ -1,2 +1,40 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+/* **************************************************
+ * Introduction
+ * ************************************************** */
 Console.WriteLine("Hello, World!");
+Console.WriteLine();
+Console.WriteLine($"{DateTime.Now.ToString(CW.CCouch.SharedKernel.Formatting.Constants.FullDateTimeFormat)} Begin...");
+Console.WriteLine();
+
+/* ************************************************** */
+
+/* **************************************************
+ * Do Work
+ * ************************************************** */
+
+// RGB To Hex Conversion Exercise
+var rgbToHexConversionOutput =
+    CW.CCouch.Seeding.Exersices.RGBToHexConversion.Exercise();
+
+Console.WriteLine($"{rgbToHexConversionOutput}");
+Console.WriteLine();
+
+// Sorting Integer Collection Exercise
+var sortingInegerCollectionOutput =
+    CW.CCouch.Seeding.Exersices.SortingIntegerCollection.Exercise();
+
+Console.WriteLine($"{sortingInegerCollectionOutput}");
+Console.WriteLine();
+
+/* ************************************************** */
+
+/* **************************************************
+ * Conclusion 
+ * ************************************************** */
+Console.WriteLine($"{DateTime.Now.ToString(CW.CCouch.SharedKernel.Formatting.Constants.FullDateTimeFormat)} End...");
+Console.WriteLine();
+Console.WriteLine("Goodbye!");
+
+/* ************************************************** */
